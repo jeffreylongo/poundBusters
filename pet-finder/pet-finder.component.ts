a41cce57c19259c747fc928b4f7a09855be803dd
Jeff Longo <jefflongo@Jeffs-MacBook-Pro.local>
2020-09-23 15:52:48 -0400

    sync

--- pet-finder/pet-finder.component.ts
+++ pet-finder/pet-finder.component.ts
@@ -16,18 +16,34 @@ export class PetFinderComponent implements OnInit {
   ngOnInit() {
     this.petFinderTokenizer.getToken().subscribe(
       (data) => {
-        console.log(`token is ${data}`);
-        localStorage.setItem('token', data.token);
-        localStorage.setItem('tokenexpiration', data.expiration);
+        console.log(`token is ${data.access_token}`);
+        localStorage.setItem('token', data.access_token);
+        localStorage.setItem('tokenexpiration', data.expires_in);
       },
       error => {
         console.log(`something wrong ${error}`);
       }
     );
-    // this.petFinderService.getPets()
-    //   .subscribe((data: any) => {
-    //     this.pets = data.results;
+    this.petFinderService.getPets()
+      .subscribe((data: any) => {
+        this.pets = data.animals;
+        if (data.pagination != null) {
+          console.log(`there are ${data.pagination.total_pages} pages of animals`);
+        }
+        console.log(this.pets);
+      });
+    // this.getArticles(url: string, articles: any[]) {
+    //   this.httpClient.get(url).subscribe(data => {
+    //     if (articles === undefined) { articles = data['articles']; } else { articles = articles.concat(data['articles']); }
+    //     if (data['next_page'] != null) {
+    //       this.getArticles(data['next_page'], articles);
+    //     } else { console.log('Finished'); }
+    //     this.articles = articles;
     //   });
+    // }
   }
 
 }
+
+
+
